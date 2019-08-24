33c5eba0e6902a25c00784ca9e5c590f117d3022
Robert-Mellberg <52757362+Robert-Mellberg@users.noreply.github.com>
2019-08-24 23:20:43 +0200

    Create SimonSays(1,5p).cpp

---
+++ Easy (1-3p)/SimonSays(1,5p).cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <vector>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <map>
+#include <bitset>
+#include <math.h>
+#include <cmath>
+#include <algorithm>
+#include <stack>
+using namespace std;
+
+//https://open.kattis.com/problems/simonsays
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int cases;
+    cin >> cases;
+    cin.ignore();
+    for (int i = 0; i < cases; i++) {
+        string line;
+        getline(cin, line);
+
+        stringstream ss(line);
+        string word;
+        ss >> word;
+        if (word == "Simon") {
+            if (ss >> word && word == "says") {
+                if (ss >> word) {
+                    cout << word;
+                    while (ss >> word) {
+                        cout << " " << word;
+                    }
+                    cout << endl;
+                }
+            }
+        }
+    }
+    
+
+    return 0;
+}
