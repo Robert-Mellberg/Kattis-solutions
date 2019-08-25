714d1f02e69d4c005e4f37dd49c103259f5a1c32
Robert-Mellberg <52757362+Robert-Mellberg@users.noreply.github.com>
2019-08-25 19:17:44 +0200

    Create QuickEstimates(1,5p).cpp

---
+++ Easy (1-3p)/QuickEstimates(1,5p).cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <vector>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <map>
+#include <bitset>
+#include <math.h>
+#include <cmath>
+#include <algorithm>
+#include <stack>
+using namespace std;
+
+//https://open.kattis.com/problems/quickestimate
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int cases;
+    cin >> cases;
+    for (int i = 0; i < cases; i++) {
+        string input;
+        cin >> input;
+        cout << input.length() << endl;
+    }
+
+    
+    return 0;
+}
